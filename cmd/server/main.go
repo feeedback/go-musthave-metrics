275f5222eaeb91c7f613fdfdf10579656417c84b
Aleksandr Rzhanitsyn <risdeep@gmail.com>
2024-04-30 02:00:24 +0400

    feat(internal\handlers): add draft server handler

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/feeedback/go-musthave-metrics/internal/handlers"
+)
+
+func main() {
+	if err := run(); err != nil {
+		panic(err)
+	}
+}
+
+func run() error {
+	return http.ListenAndServe(`:8080`, http.HandlerFunc(handlers.Webhook))
+}
---
+++ internal/handlers/handlers.go
@@ -0,0 +1,22 @@
+package handlers
+
+import "net/http"
+
+// функция webhook — обработчик HTTP-запроса
+func Webhook(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	_, _ = w.Write([]byte(`
+      {
+        "response": {
+          "text": "Извините, я пока ничего не умею"
+        },
+        "version": "1.0"
+      }
+    `))
+}
